2ce4028caaba6465e257cab814981db148ca53e0
johnroberts <robertsjr.john.c41@gmail.com>
2022-05-31 11:14:53 -0500

    upload table exercises

---
+++ tables_exercises.sql
@@ -0,0 +1,16 @@
+SHOW DATABASES;
+USE employees;
+SHOW TABLES; 
+
+DESCRIBE employees;        
+-- 5. What different data types are present on this table? Employee number, birth date, first name, last name, gender, hire date are the data types. 
+-- 6. Which table(s) do you think contain a numeric type column? Employee number
+-- 7. Which table(s) do you think contain a string type column? That would be first and last name. 
+-- 8. Which table(s) do you think contain a date type column? Hire date and birth date
+-- 9. What is the relationship between the employees and the departments tables? Basically contain the same information except the different department worked in.
+ SHOW CREATE TABLE dept_manager;
+
+
+
+
+ 
